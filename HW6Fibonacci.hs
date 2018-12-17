e35a6b4a1d9ec9b5ab6eeb2b33b85b30d822bfff
Mamtaj Akter <mamtajakter@Mamtajs-MacBook-Pro.local>
2018-12-17 00:43:21 -0800

    1-6, except 4.1,4.4,5.5,5.6,6.3,6.5,6.6,6.7

--- HW6Fibonacci.hs
+++ HW6Fibonacci.hs
@@ -1,28 +1,38 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+
 module Fibonacci where
 
 -- Homework 6
 
-
+--1--
 fib :: Integer -> Integer
 fib 0=0
 fib 1=1
 fib x= fib (x-1)+fib (x-2)
 
 fibs1 :: [Integer]
-fibs1 = [x..]
-       where x= fib x
+fibs1 =  map fib [0..]
+
+--2--found in stack overflow
 
 fibs2 :: [Integer]
 fibs2 = 0 : 1 : 1 : zipWith (+) fibs2 (tail fibs2)
 
+--3-- (streamToList 1) gives error :(
 data Stream a= Cons a (Stream a)
 
 streamToList :: Stream a -> [a]
 streamToList (Cons x y)= x : streamToList y
 
+--ListOfTwentyStream= take 20 (streamToList s)
+
 instance Show a => Show (Stream a) where
-          show s@(Cons x y)= show z
-                          where z= take 20 (streamToList s)
+          show = show . take 20 . streamToList
+
+
+--4--
+
 streamRepeat :: a -> Stream a
 streamRepeat x= Cons x (streamRepeat x)
 
@@ -31,6 +41,9 @@ streamMap f (Cons x y) = Cons (f x) (streamMap f y)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
 streamFromSeed f x = Cons x (streamFromSeed f (f x))
+
+
+--5--
 
 nats :: Stream Integer
 nats = Cons 0 (streamInfinite 1)
@@ -41,5 +54,10 @@ streamInfinite x= Cons x (streamInfinite (x+1))
 --ruler :: Stream Integer
 --ruler
 
-streamInfinite' :: Integer -> Stream Integer
-streamInfinite' x= Cons x (streamInfinite (x+1))
+--6--
+
+x :: Stream Integer
+x= Cons 0 (streamSequence 1)
+
+streamSequence :: Integer -> Stream Integer
+streamSequence y= Cons 1 (streamSequence (y+1))
