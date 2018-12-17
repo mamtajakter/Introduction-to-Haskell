20b665752ad7a6471ad18f090fb0943be9f184dc
Mamtaj Akter <mamtajakter@Mamtajs-MacBook-Pro.local>
2018-12-16 21:16:26 -0800

    Exercise 1-5 except 4.1,4.4,5.5,5.6

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -5,26 +5,21 @@ module Calc where
 
 import ExprT
 import Parser
+import StackVM
 import qualified Data.Map as M
 
-newtype MinMax = MinMax Integer deriving (Show, Eq, Ord)
-newtype Mod7   = Mod7   Integer deriving (Show, Eq)
 
+
+--1--
 
 eval :: ExprT-> Integer
 eval (Lit a)= a
-eval (Add a b)= case (a,b) of
-                (Lit x, Add y z)  -> x + (eval (Add y z))
-                (Lit x, Mul y z)  -> x+ (eval (Mul y z))
-                (Add y z, Lit x)  -> (eval (Add y z)) + x
-                ( Mul y z, Lit x)  -> (eval (Mul y z)) + x
-                (Lit x,Lit y)      -> x+y
-eval (Mul a b)= case (a,b) of
-                (Lit x, Add y z)  -> x * (eval (Add y z))
-                (Lit x, Mul y z)  -> x* (eval (Mul y z))
-                (Add y z, Lit x)  -> (eval (Add y z)) * x
-                (Mul y z, Lit x)  -> (eval (Mul y z)) * x
-                (Lit x,Lit y)      -> x*y
+eval (Add a b)= eval a + eval b
+eval (Mul a b)= eval a * eval b
+
+
+
+--2--
 
 evalStr:: String-> Maybe Integer
 evalStr x= case y of
@@ -32,6 +27,8 @@ evalStr x= case y of
         Just n-> Just (eval n)
        where y=parseExp Lit Add Mul x
 
+
+--3--
 
 class Expr a where
     lit :: Integer-> a
@@ -41,18 +38,16 @@ class Expr a where
 
 instance Expr ExprT where
    lit x = Lit x
-   add a b = case (a,b) of
-          (Lit x, Add y z)  ->  add (Lit x) (add y z)
-          (Lit x, Mul y z)  ->  add (Lit x) (mul y z)
-          (Add y z, Lit x)  -> add (add y z) (Lit x)
-          ( Mul y z, Lit x)  -> add (mul y z) (Lit x)
-          (Lit x,Lit y)      -> Lit (x+y)
-   mul a b = case (a,b) of
-          (Lit x, Add y z)  ->  mul (Lit x) (add y z)
-          (Lit x, Mul y z)  ->  mul (Lit x) (mul y z)
-          (Add y z, Lit x)  ->  mul (add y z) (Lit x)
-          ( Mul y z, Lit x)  -> mul (mul y z) (Lit x)
-          (Lit x,Lit y)      -> Lit (x*y)
+   add a b = Add a b
+   mul a b = Mul a b
+
+
+reify :: ExprT-> ExprT
+reify= id
+
+--4--
+
+
 
 instance Expr Integer where
    lit x= x
@@ -67,6 +62,11 @@ instance Expr Bool where
    mul a b = a && b
 
 
+
+newtype MinMax = MinMax Integer deriving (Show, Eq)
+newtype Mod7   = Mod7   Integer deriving (Show, Eq)
+
+
 instance Expr MinMax where
    lit a = MinMax a
    add (MinMax a) (MinMax b)=  MinMax (max a b)
@@ -74,19 +74,14 @@ instance Expr MinMax where
 
 
 instance Expr Mod7 where
-   lit x = Mod7 x
+   lit x = Mod7 ( x `mod` 7)
    add (Mod7 a) (Mod7 b)= Mod7 ((a+b) `mod` 7)
    mul (Mod7 a) (Mod7 b)= Mod7 ((a*b) `mod` 7)
 
-{-
-instance Expr Program where
-   lit x = (PushI x) :[]
-   add (PushI a) (PushI b)= PushB (a+b) :[]
-   mul (PushI a) (PushI b)= PushB (a*b) :[]
--}
+
 
 testExp :: Expr a => Maybe a
-testExp = parseExp lit add mul "(3 * 4) + 5"
+testExp = parseExp lit add mul "(3 * -4) + 5"
 
 
 testInteger = testExp :: Maybe Integer
@@ -95,7 +90,20 @@ testMM = testExp :: Maybe MinMax
 testSat = testExp :: Maybe Mod7
 --testInteger = parseExp lit add mul "(3 * 4) + 5"
 
+--5-- i  dont understand how to create instance of [stackExp], so I gave it up
 
+--newtype Program = Program { unString16 :: [Word16] }
+{-
+instance Expr Program where
+   lit x = (PushI x) :[]
+   add (PushI a) (PushI b)= PushB (a+b) :[]
+   mul (PushI a) (PushI b)= PushB (a*b) :[]
+
+
+--compile:: String-> Maybe Program
+-}
+
+--6-- I am stuck again, dont understand how to write  instance of function
 class HasVars a where
     var :: String-> a
 
@@ -110,26 +118,28 @@ data VarExprT = VarLit Integer
 
 instance Expr VarExprT where
    lit x = VarLit x
-   add a b = case (a,b) of
-          (VarLit x, VarAdd y z)  ->  add (VarLit x) (add y z)
-          (VarLit x, VarMul y z)  ->  add (VarLit x) (mul y z)
-          (VarAdd y z, VarLit x)  -> add (add y z) (VarLit x)
-          ( VarMul y z, VarLit x)  -> add (mul y z) (VarLit x)
-          (VarLit x,VarLit y)      -> VarLit (x+y)
-          (VarLit x,Var y)      -> Var ( show x ++ "+"++ y)
-          (Var x,VarLit y)      -> Var (  x ++ "+" ++  show y)
-   mul a b = case (a,b) of
-          (VarLit x, VarAdd y z)  ->  mul (VarLit x) (add y z)
-          (VarLit x, VarMul y z)  ->  mul (VarLit x) (mul y z)
-          (VarAdd y z, VarLit x)  -> mul (add y z) (VarLit x)
-          ( VarMul y z, VarLit x)  -> mul (mul y z) (VarLit x)
-          (VarLit x,VarLit y)      -> VarLit (x*y)
-          (VarLit x,Var y)      -> Var ( show x ++ "*"++ y)
-          (Var x,VarLit y)      -> Var (  x ++ "*" ++  show y)
+   add a b = VarAdd a b
+   mul a b = VarMul a b
 
 instance HasVars VarExprT where
    var s= Var s
 
+instance Expr  (M.Map String Integer-> Maybe Integer) where
+   lit x = f (M.Map s x)
+
+instance HasVars  (M.Map String Integer-> Maybe Integer) where
+   var x = M.lookup x
+
+
+{-
 type Name =String
 type PhoneNum= String
 type PhoneBook =M.Map Name PhoneNum
+
+book :: PhoneBook
+book = M.fromList [("A", "1"), ("B","2")]
+
+book'= M.insert "C" "3" book
+
+lookupTwo n1 n2 bk= (M.lookup n1 bk, M.lookup n2 bk)
+-}
